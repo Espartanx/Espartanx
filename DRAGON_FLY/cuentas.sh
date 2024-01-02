d4998f5bb3f617f6e3cfffa7c17842108fd49dad
DRAGON_FLY <119144727+Espartanx@users.noreply.github.com>
2024-01-01 18:36:55 -0600

    Add files via upload

---
+++ DRAGON_FLY/cuentas.sh
@@ -0,0 +1,455 @@
+#!bin/bash/
+
+
+#inicio 
+clear
+sleep 2
+
+# Variables con los códigos de colores
+colorn='\033[30m'
+colorr='\033[31m'
+colorv='\033[32m'
+coloram='\033[33m'
+colora='\033[34m'
+colorm='\033[35m'
+colort='\033[36m'
+colorb='\033[37m'
+fondon='\033[40m'
+fondor='\033[41m'
+fondov='\033[42m'
+fondoam='\033[43m'
+fondoa='\033[44m'
+fondom='\033[45m'
+fondot='\033[46m'
+fondog='\033[47m'
+colorset='\033[0m'
+
+#aqui ira el logo mio 
+
+echo -e ${colorr}
+echo              '  ____                                _     _ _       '
+echo -e ${colora} ' |  _ \ _ __ __ _  __ _  ___  _ __   | |__ (_) |_ ___ '
+echo -e ${colorb} ' | | | |_ __/ _` |/ _` |/ _ \|-- _ \  | _ \| | __/ _ \ ' 
+echo -e ${colort} ' | |_| | | | (_| | (_| | (_) | | | | | |_) | | ||  __/ ' 
+echo -e ${coloram}'  |____/|_|  \__,_|\__, |\___/|_| |_| |_.__/|_|\__\___| ' 
+echo -e ${colorv} '                  |___/                                '
+                           echo -e ${colorr} 'by:espartan'
+sleep 1
+ #linea de colores 
+printf "\e[38;5;196m_"
+sleep 0.1
+   printf "\e[38;5;202m_"
+sleep 0.1
+   printf "\e[38;5;208m_"
+sleep 0.1
+   printf "\e[38;5;214m_"
+sleep 0.1
+   printf "\e[38;5;220m_"
+sleep 0.1
+   printf "\e[38;5;226m_"
+sleep 0.1
+   printf "\e[38;5;190m_"
+sleep 0.1
+   printf "\e[38;5;154m_"
+sleep 0.1
+   printf "\e[38;5;118m_"
+sleep 0.1
+   printf "\e[38;5;82m_"
+sleep 0.1
+  printf "\e[38;5;46m_"
+sleep 0.1
+   printf "\e[38;5;47m_"
+sleep 0.1
+   printf "\e[38;5;48m_"
+sleep 0.1
+   printf "\e[38;5;49m_"
+sleep 0.1
+   printf "\e[38;5;50m_"
+sleep 0.1
+   printf "\e[38;5;51m_"
+sleep 0.1
+   printf "\e[38;5;45m_"
+sleep 0.1
+   printf "\e[38;5;39m_"
+sleep 0.1
+   printf "\e[38;5;33m_"
+sleep 0.1
+   printf "\e[38;5;27m_"
+sleep 0.1
+   printf "\e[38;5;21m_"
+sleep 0.1
+   printf "\e[38;5;57m_"
+sleep 0.1
+   printf "\e[38;5;93m_"
+sleep 0.1
+   printf "\e[38;5;129m_"
+sleep 0.1
+   printf "\e[38;5;165m_"
+sleep 0.1
+   printf "\e[38;5;201m_"
+sleep 0.1
+   printf "\e[38;5;200m_"
+sleep 0.1
+   printf "\e[38;5;199m_"
+sleep 0.1
+   printf "\e[38;5;198m_"
+sleep 0.1
+   printf "\e[38;5;197m_"
+sleep 0.1
+printf "\e[38;5;196m_"
+sleep 0.1
+   printf "\e[38;5;202m_"
+sleep 0.1
+   printf "\e[38;5;208m_"
+sleep 0.1
+   printf "\e[38;5;214m_"
+sleep 0.1
+   printf "\e[38;5;220m_"
+sleep 0.1
+   printf "\e[38;5;226m_"
+sleep 0.1
+   printf "\e[38;5;190m_"
+sleep 0.1
+   printf "\e[38;5;154m_"
+sleep 0.1
+   printf "\e[38;5;118m_"
+sleep 0.1
+   printf "\e[38;5;82m_"
+sleep 0.1
+   printf "\e[38;5;46m_"
+sleep 0.1
+   printf "\e[38;5;47m_"                                             
+sleep 0.1
+printf "\e[38;5;48m_"
+sleep 0.1
+   printf "\e[38;5;49m_"
+sleep 0.1
+   printf "\e[38;5;50m_"
+sleep 0.1
+   printf "\e[38;5;51m_"
+sleep 0.1
+   printf "\e[38;5;45m_"
+sleep 0.1
+   printf "\e[38;5;39m_"
+sleep 0.1
+   printf "\e[38;5;33m_"
+sleep 0.1
+   printf "\e[38;5;27m_"
+sleep 0.1
+   printf "\e[38;5;21m_"
+sleep 0.1
+   printf "\e[38;5;57m_"
+sleep 0.1
+printf "\e[38;5;93m_"
+sleep 0.1
+   printf "\e[38;5;129m_"
+sleep 0.1
+   printf "\e[38;5;165m_"
+sleep 0.1
+   printf "\e[38;5;201m_"
+sleep 0.1
+   printf "\e[38;5;200m_"
+sleep 0.1
+   printf "\e[38;5;199m_"
+sleep 0.1
+   printf "\e[38;5;198m_"
+sleep 0.1
+   printf "\e[38;5;197m_"
+sleep 0.1
+printf "\e[38;5;196m_"
+sleep 0.1
+   printf "\e[38;5;202m_"
+sleep 0.1
+   printf "\e[38;5;208m_"
+sleep 0.1
+   printf "\e[38;5;214m_"
+sleep 0.1
+   printf "\e[38;5;220m_"
+sleep 0.1
+   printf "\e[38;5;226m_"
+sleep 0.1
+   printf "\e[38;5;190m_"
+sleep 0.1
+   printf "\e[38;5;154m_"
+sleep 0.1
+   printf "\e[38;5;118m_"
+sleep 0.1
+   printf "\e[38;5;82m_"
+sleep 0.1
+   printf "\e[38;5;46m_"
+sleep 0.1
+   printf "\e[38;5;47m_"
+sleep 0.1
+   printf "\e[38;5;48m_"
+sleep 0.1
+   printf "\e[38;5;49m_"
+sleep 0.1
+   printf "\e[38;5;50m_"
+sleep 0.1
+   printf "\e[38;5;51m_"
+sleep 0.1
+   printf "\e[38;5;45m_"
+sleep 0.1
+   printf "\e[38;5;39m_"
+sleep 0.1
+   printf "\e[38;5;33m_"
+sleep 0.1
+   printf "\e[38;5;27m_"
+sleep 0.1
+   printf "\e[38;5;21m_"
+sleep 0.1
+   printf "\e[38;5;57m_"
+sleep 0.1
+   printf "\e[38;5;93m_"
+sleep 0.1
+   printf "\e[38;5;129m_"
+sleep 0.1
+   printf "\e[38;5;165m_"
+sleep 0.1
+   printf "\e[38;5;201m_"
+sleep 0.1
+   printf "\e[38;5;200m_"
+sleep 0.1
+   printf "\e[38;5;199m_"
+sleep 0.1
+   printf "\e[38;5;198m_"
+sleep 0.1
+   printf "\e[38;5;197m_"
+sleep 0.1
+   printf "\e[0m\n"
+sleep 0.1
+#aqui ira el de subcribance
+echo -e ${colorr} "░█▀▀░█░█░█▀▄░█▀▀░█▀▀░█▀▄░▀█▀░█▀▄░█▀▀░▀█▀░█▀▀░░"
+echo -e ${colorb} "░▀▀█░█░█░█▀▄░▀▀█░█░░░█▀▄░░█░░█▀▄░█▀▀░░█░░█▀▀░░"
+echo -e ${colora} "░▀▀▀░▀▀▀░▀▀░░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀▀░░▀▀▀░░▀░░▀▀▀░░"
+echo "                                            "
+echo "                                              "
+sleep 2                                              
+
+
+
+
+
+
+#aqui ab1ira youtube
+
+termux-open-url https://www.youtube.com/@dragonflybyte
+
+# espere 
+echo "espere 5 seg"
+sleep 1
+clear
+echo "4"
+sleep 1
+clear
+echo "3"
+sleep 1
+clear
+echo "2" 
+sleep 1
+clear
+echo "listo"
+sleep 1
+clear
+
+#logo 
+while IFS= read -r line; do
+    color=$((RANDOM % 8 + 30))
+ # Genera un número aleatorio entre 30 y 37 para el código de color
+    echo -e "\e[${color}m${line}\e[0m"
+# Imprime el texto con el código de color y luego restablece el color a la normalidad
+    sleep 0.2
+# Espera 0.2 segundos antes de leer la siguiente línea
+done < logo1.txt
+#seleccion de  variables 
+     #colores
+color_reset="\033[0m"
+sleep 0.1
+echo "Elige una opción:"
+sleep 1
+opciones() {
+  echo -e ${colorr} "1. Netflix"
+ sleep 0.5
+   echo -e ${colora} "2. Paramount+"
+sleep 0.4
+    echo -e ${colorv} "3. HBO+"
+  sleep 0.5
+  echo -e ${coloram}"4. Salir"
+  sleep 0.5
+}
+
+eleccion() {
+  read -p "Elige una opción: " opcion
+  case $opcion in
+    1)
+      echo -e ${colorr}
+      echo '  _   _      _    __ _ _ '
+      echo ' | \ | |    | |  / _| (_) '
+      echo ' |  \| | ___| |_| |_| |___  __ '
+      echo ' | . ` |/ _ \ __|  _| | \ \/ /   '
+      echo ' | |\  |  __/ |_| | | | |>  <  '
+      echo ' |_| \_|\___|\__|_| |_|_/_/\_\ ' 
+      echo -e ${color_reset}
+      echo -e ${colorm}'by:espartan'
+      echo -e ${color_reset}
+      sleep 1
+      echo "Aquí están las cuentas de Netflix"
+      sleep 1
+      clear
+      sleep 0.5
+      echo "listo"
+      sleep 0.5
+      while IFS= read -r line; do
+    color=$((RANDOM % 8 + 30))
+ # Genera un número aleatorio entre 30 y 37 para el código de color
+    echo -e "\e[${color}m${line}\e[0m"
+# Imprime el texto con el código de color y luego restablece el color a la normalidad
+    sleep 0.2
+# Espera 0.2 segundos antes de leer la siguiente línea
+done < netflix.txt
+# Función para mostrar la cuenta regresiva en una sola línea
+function cuenta_regresiva() {
+    local segundos=$1
+    while [ $segundos -gt 0 ]; do
+        printf "Tiempo restante: %s segundos\r" "$segundos"
+        sleep 1
+        segundos=$((segundos - 1))
+    done
+    printf "\n"
+}
+
+# Iniciar la cuenta regresiva
+cuenta_regresiva 300      
+      sleep 1
+      echo  ""
+      echo "disfruta las cuentas"
+      echo "preciona"
+      echo -e "${colorr}crl${color_reset}"
+      echo -e "${colorb}+${color_reset}"
+      echo -e "${colorr}C${color_reset}"
+      echo "para salir"
+      echo "volviendo al menu"
+     clear
+      ;;
+    2)
+
+echo -e ${colorb} "                              ███▓░                                '"
+sleep 0.1
+echo -e ${colorr} "                             ███████▓                              "
+sleep 0.1
+echo -e ${colorv} "                            ██████▓██░                            "
+sleep 0.1
+echo -e ${colora} "                            █▒▓██▒░   ██                           "
+sleep 0.1
+echo -e ${coloram}"                         ░██░░▒▒▓▓█▓████▓                          "
+sleep 0.1
+echo -e ${colorb} "                        ▓██ ░░     ▒█████                         "
+sleep 0.1
+echo -e ${colorn} "                       ▒█░          ██████                        "
+sleep 0.1
+echo -e ${colorr} "                      ░█░          ▒███████                       "
+sleep 0.1
+echo -e ${colort} "                     ██             ███████                       "
+sleep 0.1
+echo -e ${colorv} "                                 ░    ███▓▒█▓                     "
+sleep 0.1
+echo -e ${colorb} "                                     █████████                    "
+sleep 0.1
+echo -e ${colorr} "                ░                  ▓█▓█████▒▒█████               "
+sleep 0.1
+echo -e ${colora} "           ░▒                     ░░█▒▓█▓█▒  ▒████░             "
+sleep 0.1
+echo -e ${coloram}"             ░                     ░█▒█████▓▒▓█▒██████▓            "
+sleep 0.1
+echo -e ${colorv} "                            ████░▓███░█████ ░░▓████████░          "
+sleep 0.1
+echo -e ${colora} "           ░  ██████         ▓█████ █▓█▓▓███▓░█▓░░▒██████▓        "
+sleep 0.1
+echo -e ${colorb} "        ░████████                   ░░▒▒░██▓█       ▒█▓███▓      "
+sleep 0.1
+echo -e ${colorn} "     ░▓██████▓░       ▒░           ░   ░██░           ░░░░░░░     "
+sleep 0.1
+echo -e ${colorv} " ████▓▓▒▒▒▒██▒     ░▓████▓          ░ ░░  ░░░ ░░            ░▒▒       "
+                 sleep 2
+      echo "Aquí están las cuentas de Paramount+"
+echo -e ${color_reset}
+      sleep 0.5
+      clear
+      sleep 0.5
+      while IFS= read -r line; do
+    color=$((RANDOM % 8 + 30))
+ # Genera un número aleatorio entre 30 y 37 para el código de color
+    echo -e "\e[${color}m${line}\e[0m"
+# Imprime el texto con el código de color y luego restablece el color a la normalidad
+    sleep 0.2
+# Espera 0.2 segundos antes de leer la siguiente línea
+done < paramount.txt
+# Función para mostrar la cuenta regresiva en una sola línea
+function cuenta_regresiva() {
+    local segundos=$1
+    while [ $segundos -gt 0 ]; do
+        printf "Tiempo restante: %s segundos\r" "$segundos"
+        sleep 1
+        segundos=$((segundos - 1))
+    done
+    printf "\n"
+}
+
+# Iniciar la cuenta regresiva
+cuenta_regresiva 300
+      ;;
+    3)
+   sleep 2
+echo -e${colorr}  "    _    _ ____   ____          "
+echo -e ${colorm} " | |  | |  _ \ / __ \     _   "
+echo              "  | |__| | |_) | |  | |  _| |_ "
+echo              "  |  __  |  _ <| |  | | |_   _|"
+echo              "  | |  | | |_) | |__| |   |_|  "
+echo              "  |_|  |_|____/ \____/         "
+echo "                               "
+echo -e  ${color_reset} "                              "
+      echo "by: espartan" 
+      sleep 3
+      echo "Aquí están las cuentas de HBO+"
+      sleep 0.5
+      clear
+      sleep 0.5
+      while IFS= read -r line; do
+    color=$((RANDOM % 8 + 30))
+ # Genera un número aleatorio entre 30 y 37 para el código de color
+    echo -e "\e[${color}m${line}\e[0m" 
+# Imprime el texto con el código de color y luego restablece el color a la normalidad
+    sleep 0.2 
+# Espera 0.2 segundos antes de leer la siguiente línea
+done < hbo.txt
+# Función para mostrar la cuenta regresiva en una sola línea
+function cuenta_regresiva() {
+    local segundos=$1
+    while [ $segundos -gt 0 ]; do
+        printf "Tiempo restante: %s segundos\r" "$segundos"
+        sleep 1
+        segundos=$((segundos - 1))
+    done
+    printf "\n"
+}
+
+# Iniciar la cuenta regresiva
+cuenta_regresiva 300
+      ;;
+    4)
+      echo "Saliendo..."
+      sleep 1
+      clear
+      exit
+      ;;
+    *)
+      echo "Opción inválida"
+      ;;
+  esac
+}
+
+while true; do
+  opciones
+  eleccion
+done
+
